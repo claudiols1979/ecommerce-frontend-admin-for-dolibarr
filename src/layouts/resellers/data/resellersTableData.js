0ea4862a9555779740afaf6e2c2db8650cc37cdf
Claudio Lopez <claudiols1979@hotmail.com>
2025-06-12 17:55:22 -0600

    Update

--- src/layouts/resellers/data/resellersTableData.js
+++ src/layouts/resellers/data/resellersTableData.js
@@ -1,5 +1,5 @@
 // frontend/src/layouts/resellers/data/resellersTableData.js
-
+/* eslint-disable */
 import React from "react";
 import { Link } from "react-router-dom"; // Import Link for navigation
 import PropTypes from "prop-types";
