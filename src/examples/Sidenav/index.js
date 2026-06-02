0bd2bdf23397fc9098d8e0eee99de2014ddcc1c4
Claudio Lopez <claudiols1979@hotmail.com>
2026-06-02 12:56:07 -0600

    update
    update

--- src/examples/Sidenav/index.js
+++ src/examples/Sidenav/index.js
@@ -179,7 +179,7 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
           </MDTypography>
         </MDBox>
         <MDBox component={NavLink} to="/" display="flex" alignItems="center">
-          {brand && <MDBox component="img" src={brand} alt="Brand" width="2rem" />}
+          {brand && <MDBox component="img" src={brand} alt="Brand" width="12rem" />}
           <MDBox
             width={!brandName && "100%"}
             sx={(theme) => sidenavLogoLabel(theme, { miniSidenav })}
--- src/routes.js
+++ src/routes.js
@@ -55,7 +55,7 @@ const routes = [
     type: "collapse",
     name: "Panel",
     key: "dashboard",
-    icon: <Icon sx={{ fontSize: { xs: "2.2rem", md: "3rem" } }}>dashboard</Icon>,
+    icon: <Icon sx={{ fontSize: "6rem" }}>dashboard</Icon>,
     route: "/dashboard",
     component: <Dashboard />,
     // Roles with double quotes for Prettier compliance
